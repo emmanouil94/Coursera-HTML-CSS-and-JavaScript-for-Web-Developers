190967ece0d475f910981561f50c0d65bd739d4b
emmanouil94 <76157934+emmanouil94@users.noreply.github.com>
2020-12-18 11:53:40 +0200

    Create script.js

---
+++ module4-solution/script.js
@@ -0,0 +1,16 @@
+(function () {
+
+var names = ["Emmanouil", "Chris", "Bob", "Alice", "Frank", "Michael", "Rowdie", "Cecilia", "Helen", "Karen"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
