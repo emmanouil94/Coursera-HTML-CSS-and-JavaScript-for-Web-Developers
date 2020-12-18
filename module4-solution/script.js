65ac70e0d9099b114f89f688c60e127bb0ca5715
emmanouil94 <76157934+emmanouil94@users.noreply.github.com>
2020-12-18 12:04:41 +0200

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,11 +1,11 @@
 (function () {
 
-var names = ["Emmanouil", "Chris", "Bob", "Alice", "Frank", "Michael", "Rowdie", "Cecilia", "Helen", "Karen"];
+var names = ["Emmanouil", "Jane", "Bob", "Jack", "Frank", "Jim", "Rowdie", "Cecilia", "Jaren", "Karen"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
 
-  if (firstLetter === 'c' ) {
+  if (firstLetter === 'j' ) {
     byeSpeaker(names[i]);
   }
   else {
