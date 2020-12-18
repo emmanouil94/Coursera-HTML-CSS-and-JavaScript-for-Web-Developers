535da585e9507fc086fe1531778909b441b4dfbd
emmanouil94 <76157934+emmanouil94@users.noreply.github.com>
2020-12-18 12:15:47 +0200

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,16 +1,16 @@
-(function () {
-
-var names = ["Emmanouil", "Jane", "Bob", "Jack", "Frank", "Jim", "Rowdie", "Cecilia", "Jaren", "Karen"];
-
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter == 'J' ||  firstLetter == 'j' ) {
-    byeSpeaker(names[i]);
-  }
-  else {
-    helloSpeaker(names[i]);
-  }
-}
-
+(function() 
+ {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+  for (var i = 0; i < names.length; i++) 
+  {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        
+    if (firstLetter === 'j' || firstLetter === 'J') {
+            byeSpeaker.speak(names[i]);
+        } 
+    else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
 })();
